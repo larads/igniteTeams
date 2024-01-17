61be87e062c8d5bc3e34d73421f8b86ca8c99595
Mariana da Silva <dasilval.mariana@gmail.com>
2024-01-17 11:48:41 -0400

    🌈 style(CSS-in-JS - Styling a StatusBar):

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from 'react-native'
 import { ThemeProvider } from 'styled-components'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { Loading } from '@components/Loaded'
@@ -10,6 +11,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
       {fontsLoaded ? <Groups/> :  <Loading/> }
     </ThemeProvider>
   )
