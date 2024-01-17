0dc9d49dc6d00c688fd81488184879c809cf8b8e
Mariana da Silva <dasilval.mariana@gmail.com>
2024-01-17 10:33:08 -0400

    🌈 style(CSS-in-JS - Using theme):

---
+++ src/theme/index.ts
@@ -0,0 +1,29 @@
+export default {
+    Colors: {
+        White: '#FFFF',
+
+        Green_700: '#00875F',
+        Green_500: '#00B37E',
+
+        Red: '#F75A68',
+        Red_Dark: '#AA2834',
+
+        Gray_700: '#121214',
+        Gray_600: '#202024',
+        Gray_500: '#29292E',
+        Gray_400: '#323238',
+        Gray_300: '#7C7C8A',
+        Gray_200: '#C4C4CC',
+        Gray_100: '#E1E1E6'
+    },
+    Font_Family: {
+        Regular: 'Roboto_400Regular',
+        Bold: 'Roboto_700Bold',
+    },
+    Font_Size: {
+        SM: 14,
+        MD: 16,
+        LG: 18,
+        XL: 24
+    }
+}
