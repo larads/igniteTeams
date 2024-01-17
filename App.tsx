40ebb21663aa370fa4558c86fbd9b82026801acb
Mariana da Silva <dasilval.mariana@gmail.com>
2024-01-17 10:46:25 -0400

    🌈 style(CSS-in-JS - Custom Fonts):

--- App.tsx
+++ App.tsx
@@ -1,7 +1,17 @@
+import { ActivityIndicator } from 'react-native'
+import { ThemeProvider } from 'styled-components'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
+import theme from './src/theme'
+
 import { Groups } from '@screens/Groups'
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <Groups/>
+    <ThemeProvider theme={theme}>
+      { fontsLoaded ? <Groups/> :  <ActivityIndicator/> }
+    </ThemeProvider>
   )
 }
