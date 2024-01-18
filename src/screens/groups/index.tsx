edd3ca7679513926fcebe674b0cbc6e9e4ab3224
Mariana da Silva <dasilval.mariana@gmail.com>
2024-01-18 10:22:04 -0400

    🦄 refactor(list groups):

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,17 +1,31 @@
+import { useState } from 'react';
 import { Highlight } from '@components/Highlight';
 import * as S from './styles';
 import { Header } from '@components/Header';
 import { GroundCard } from '@components/GroupCard';
+import { FlatList } from 'react-native';
 
 export function Groups() {
+  const [ groups, setGroups ] = useState(['ghg', 'amigos']);
+
   return (
     <S.Container>
       <Header />
+
       <Highlight
          title='Turmas' 
          subTitle='Jogue com a turma'
       />
-      <GroundCard title='E SOBRE'  />
+
+      <FlatList 
+          data={groups} 
+          keyExtractor={item => item}
+          renderItem={({ item }) => 
+            <GroundCard 
+              title={item}
+            />}
+      />
+
     </S.Container>
   );
 }
