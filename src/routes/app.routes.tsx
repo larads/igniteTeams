3c7d5657a0a60989993d6db03db6e741eb2ad72e
Mariana da Silva <dasilval.mariana@gmail.com>
2024-01-22 09:25:00 -0400

    🌱Navigation:(Implementing Stack Navigator)

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,25 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Groups } from "@screens/Groups";
+import { Players } from "@screens/Players";
+import { NewGroup } from "@screens/NewGroup";
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AppRoutes() {
+    return (
+        <Navigator>
+            <Screen
+                name="groups"
+                component={Groups}
+            />
+            <Screen
+                name="players"
+                component={Players}
+            />
+            <Screen
+                name="new"
+                component={NewGroup}
+            />
+        </Navigator>
+    )
+}
